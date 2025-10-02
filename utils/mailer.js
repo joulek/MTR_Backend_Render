1a84f60ffa71ab671c474241a19d3055cc338599
nourhene <nourheneabbes12@gmail.com>
2025-10-02 19:53:17 +0100

    mail

--- utils/mailer.js
+++ utils/mailer.js
@@ -2,18 +2,18 @@ import nodemailer from "nodemailer";
 
 export function makeTransport() {
   const port = Number(process.env.SMTP_PORT || 587);
-  const useSecure = port === 465;
+  const secure = port === 465; // donc false ici
 
   return nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port,
-    secure: useSecure, // false pour 587
-    requireTLS: !useSecure, // true pour 587
+    secure,                 // false pour 587
+    requireTLS: !secure,    // true pour 587
     auth: { user: process.env.SMTP_USER, pass: process.env.SMTP_PASS },
-    family: 4, // force IPv4
-    connectionTimeout: 15000,
-    greetingTimeout: 10000,
-    socketTimeout: 20000,
+    family: 4,              // force IPv4 (évite soucis IPv6)
+    connectionTimeout: 20000,
+    greetingTimeout: 15000, // ↑ augmente un peu
+    socketTimeout: 25000,
     tls: { minVersion: "TLSv1.2" },
     logger: true,
     debug: true,
