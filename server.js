83332e58d63dede77a061c2d63e71aff40c35540
ynitylearn <ynitylearn@gmail.com>
2025-11-14 22:03:13 +0100

    fix front url

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.set("trust proxy", 1);
 
 /* ✅ CORS: origine(s) explicites + cookies */
 const ALLOWED_ORIGINS = [
-  "https://mtr-frontend-render.onrender.com",
+  "https://mtr-frontend-render-1.onrender.com",
   "http://localhost:3000",
 ];
 app.use(
